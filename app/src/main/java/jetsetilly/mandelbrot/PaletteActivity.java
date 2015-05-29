8047ef9e4b6154257cc5beb7a54754316bc9fc25
steve <steve@galactica.localdomain>
2015-05-29 22:25:35 +0100

    palette activity now has multiple pages. Using SlidingTabLayout and SlidingTabStrip from SlidingTabsBasic example.
    We can improve on that.
    
    TODO:
    1. other palette pages
    2. favouriting
    3. persistent storage

--- app/src/main/java/jetsetilly/mandelbrot/PaletteActivity.java
+++ app/src/main/java/jetsetilly/mandelbrot/PaletteActivity.java
@@ -2,21 +2,23 @@ package jetsetilly.mandelbrot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import jetsetilly.mandelbrot.view.SlidingTabLayout;
 
 
 public class PaletteActivity extends Activity {
-    private final String DBG_TAG = "colours activity";
+    private final String DBG_TAG = "palette activity";
 
-    public ListView lv;
-
-    private MainActivity context;
-
-    private PaletteAdapter palette_adapter;
     private PaletteControl palette_settings = PaletteControl.getInstance();
 
     @Override
@@ -24,28 +26,11 @@ public class PaletteActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
 
-        palette_adapter = new PaletteAdapter(this);
+        ViewPager pager = (ViewPager) findViewById(R.id.palette_pager);
+        pager.setAdapter(new PalettePager(this));
 
-        // add colours adapter to this list view
-        lv = (ListView) findViewById(R.id.palette_listview);
-        lv.setAdapter(palette_adapter);
-
-        // react to selections in list view
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                palette_adapter.setPaletteCard(view);
-                palette_settings.setColours(position);
-                MainActivity.render_canvas.startRender();
-            }
-        });
-
-        // scroll list to the correct location
-        lv.post(new Runnable() {
-            public void run() {
-                lv.setSelection(palette_settings.selected_id);
-            }
-        });
+        SlidingTabLayout tabs = (SlidingTabLayout) findViewById(R.id.palette_tabs);
+        tabs.setViewPager(pager);
     }
 
     @Override
@@ -73,5 +58,84 @@ public class PaletteActivity extends Activity {
     /* sets animation for going back to main activity*/
     private void setTransitionAnim() {
         overridePendingTransition(R.animator.push_down_fade_in, R.animator.push_down_fade_out);
+    }
+
+    class PalettePager extends PagerAdapter {
+        private final String DBG_TAG = "palette pager adapter";
+
+        private PaletteActivity context;
+
+        public PalettePager(PaletteActivity context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Resources r = getResources();
+
+            switch (position) {
+                case 0:
+                    return r.getString(R.string.palette_all);
+
+                case 1:
+                    return r.getString(R.string.palette_favourites);
+
+                case 2:
+                    return r.getString(R.string.palette_community);
+            }
+
+            return "*error*";
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            // Inflate a new layout from our resources
+            View view = context.getLayoutInflater().inflate(R.layout.activity_palette_page, container, false);
+            // Add the newly created View to the ViewPager
+            container.addView(view);
+
+            // TODO: implement other pages
+            if (position != 0) return view;
+
+            final PaletteAdapter palette_adapter = new PaletteAdapter(context);
+
+            // add colours adapter to this list view
+            final ListView lv = (ListView) view.findViewById(R.id.palette_listview);
+            lv.setAdapter(palette_adapter);
+
+            // react to selections in list view
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    palette_adapter.setPaletteCard(view);
+                    palette_settings.setColours(position);
+                    MainActivity.render_canvas.startRender();
+                }
+            });
+
+            // scroll list to the correct location
+            lv.post(new Runnable() {
+                public void run() {
+                    lv.setSelection(palette_settings.selected_id);
+                }
+            });
+
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }

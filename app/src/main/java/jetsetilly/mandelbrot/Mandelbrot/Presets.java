155074cf790a76ca1a54a8f71c8d08f7ee3d7f52
steve <steve@galactica.localdomain>
2015-06-17 12:15:04 +0100

    Mandelbrot settings more flexible

---
+++ app/src/main/java/jetsetilly/mandelbrot/Mandelbrot/Presets.java
@@ -0,0 +1,31 @@
+package jetsetilly.mandelbrot.Mandelbrot;
+
+public class Presets {
+    static class Preset {
+        public String name;
+        public double real_left;
+        public double real_right;
+        public double imaginary_upper;
+        public double imaginary_lower;
+        public int max_iterations;
+        public double bailout_value;
+
+        public Preset(String name, double rl, double rr, double iu, double il, int mi, double bv) {
+            this.name = name;
+            real_left = rl;
+            real_right = rr;
+            imaginary_upper = iu;
+            imaginary_lower = il;
+            max_iterations = mi;
+            bailout_value = bv;
+        }
+    }
+
+    /* default values -- these are approximately square. Mandelbrot.correctMandelbrotRange() will correct squishiness */
+    static final int DEFAULT_SETTINGS = 1;
+
+    static public Preset[] presets = {
+            new Preset("All the way out", -2.11, 1.16, 2.94, -2.88, 60, 4.0),
+            new Preset("First test of imagination", -1.8079, -1.7179, 0.0691, -0.0909, 66, 4.0),
+    };
+}
--- app/src/main/java/jetsetilly/mandelbrot/Mandelbrot/Settings.java
+++ app/src/main/java/jetsetilly/mandelbrot/Mandelbrot/Settings.java
@@ -1,14 +1,6 @@
 package jetsetilly.mandelbrot.Mandelbrot;
 
 public class Settings {
-    private final double DEF_REAL_LEFT = -2.11;
-    private final double DEF_REAL_RIGHT = 1.16;
-    private final double DEF_IMAG_UPPER = 2.94;
-    private final double DEF_IMAG_LOWER = -2.88;
-    private final int DEF_MAX_ITERATIONS = 60;
-    private final double DEF_BAILOUT_VALUE = 4.0;
-
-    /* default values -- these are approximately square. Mandelbrot.correctMandelbrotRange() will correct squishiness */
     public double real_left;
     public double real_right;
     public double imaginary_upper;
@@ -22,12 +14,12 @@ public class Settings {
     }
 
     public void resetCoords() {
-        real_left = DEF_REAL_LEFT;
-        real_right = DEF_REAL_RIGHT;
-        imaginary_upper = DEF_IMAG_UPPER;
-        imaginary_lower = DEF_IMAG_LOWER;
-        max_iterations = DEF_MAX_ITERATIONS;
-        bailout_value = DEF_BAILOUT_VALUE;
+        real_left = Presets.presets[Presets.DEFAULT_SETTINGS].real_left;
+        real_right = Presets.presets[Presets.DEFAULT_SETTINGS].real_right;
+        imaginary_upper = Presets.presets[Presets.DEFAULT_SETTINGS].imaginary_upper;
+        imaginary_lower = Presets.presets[Presets.DEFAULT_SETTINGS].imaginary_lower;
+        max_iterations = Presets.presets[Presets.DEFAULT_SETTINGS].max_iterations;
+        bailout_value = Presets.presets[Presets.DEFAULT_SETTINGS].bailout_value;
     }
 
     /* singleton pattern */

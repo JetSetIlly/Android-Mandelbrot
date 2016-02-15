a5e42d04a5bf69ce6b577d43cdac0ebaa331981e
steve <steve@galactica.localdomain>
2016-02-15 21:51:35 +0000

    mandlebrotsettings now stores doubles instead of floats

--- app/src/main/java/jetsetilly/mandelbrot/Settings/MandelbrotSettings.java
+++ app/src/main/java/jetsetilly/mandelbrot/Settings/MandelbrotSettings.java
@@ -11,10 +11,8 @@ public class MandelbrotSettings {
     public double real_right;
     public double imaginary_upper;
     public double imaginary_lower;
-
-    public int max_iterations;
     public double bailout_value;
-
+    public int max_iterations;
     public Mandelbrot.RenderMode render_mode;
 
     MandelbrotSettings() {
@@ -36,12 +34,12 @@ public class MandelbrotSettings {
         SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
         SharedPreferences.Editor prefs_editor = prefs.edit();
 
-        prefs_editor.putFloat("real_left", (float) real_left);
-        prefs_editor.putFloat("real_right", (float) real_right);
-        prefs_editor.putFloat("imaginary_upper", (float) imaginary_upper);
-        prefs_editor.putFloat("imaginary_lower", (float) imaginary_lower);
+        putDouble(prefs_editor, "real_left", (float) real_left);
+        putDouble(prefs_editor, "real_right", (float) real_right);
+        putDouble(prefs_editor, "imaginary_upper", (float) imaginary_upper);
+        putDouble(prefs_editor, "imaginary_lower", (float) imaginary_lower);
+        putDouble(prefs_editor, "bailout_value", (float) bailout_value);
         prefs_editor.putInt("max_iterations", max_iterations);
-        prefs_editor.putFloat("bailout_value", (float) bailout_value);
         prefs_editor.putInt("render_mode", render_mode.ordinal());
 
         // Commit the edits!
@@ -51,14 +49,25 @@ public class MandelbrotSettings {
     public void restore(Context context) {
         // set interface to reflect stored values
         SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
-        real_left = prefs.getFloat("real_left", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].real_left);
-        real_right = prefs.getFloat("real_right", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].real_right);
-        imaginary_upper = prefs.getFloat("imaginary_upper", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].imaginary_upper);
-        imaginary_lower = prefs.getFloat("imaginary_lower", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].imaginary_lower);
+
+        real_left = getDouble(prefs, "real_left", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].real_left);
+        real_right = getDouble(prefs, "real_right", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].real_right);
+        imaginary_upper = getDouble(prefs, "imaginary_upper", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].imaginary_upper);
+        imaginary_lower = getDouble(prefs, "imaginary_lower", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].imaginary_lower);
+        bailout_value = getDouble(prefs, "bailout_value", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].bailout_value);
         max_iterations = prefs.getInt("max_iterations", MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].max_iterations);
-        bailout_value = prefs.getFloat("bailout_value", (float) MandelbrotPresets.presets[MandelbrotPresets.DEFAULT_SETTINGS].bailout_value);
         render_mode = Mandelbrot.RenderMode.values()[prefs.getInt("render_mode", Mandelbrot.RenderMode.CENTRE.ordinal())];
     }
+
+    /* double width float support for SharedPreferences */
+    SharedPreferences.Editor putDouble(final SharedPreferences.Editor edit, final String key, final double value) {
+        return edit.putLong(key, Double.doubleToRawLongBits(value));
+    }
+
+    double getDouble(final SharedPreferences prefs, final String key, final double defaultValue) {
+        return Double.longBitsToDouble(prefs.getLong(key, Double.doubleToLongBits(defaultValue)));
+    }
+    /* end of double width float support for SharedPreferences */
 
     /* singleton pattern */
     private static final MandelbrotSettings singleton = new MandelbrotSettings();

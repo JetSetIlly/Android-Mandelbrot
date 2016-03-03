6b10eb37bb543e7ba9b0f594f8e25f20d8a22b77
steve <steve@galactica.localdomain>
2016-03-03 14:52:13 +0000

    nailed the problem of chaining zoomBy() scr

--- app/src/main/java/jetsetilly/mandelbrot/PaletteActivity.java
+++ app/src/main/java/jetsetilly/mandelbrot/PaletteActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.widget.GridView;

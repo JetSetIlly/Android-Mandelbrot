490bd8559403088a2c8e22b92c68f1929471021a
steve <steve@galactica.localdomain>
2016-08-07 10:41:08 +0100

    * SimpleAsyncTask() tidy up

--- app/src/main/java/jetsetilly/tools/SimpleAsyncTask.java
+++ app/src/main/java/jetsetilly/tools/SimpleAsyncTask.java
@@ -1,54 +1,42 @@
 package jetsetilly.tools;
 
-import android.content.Context;
 import android.os.Process;
 import android.os.AsyncTask;
-import android.support.annotation.CallSuper;
 import android.support.annotation.UiThread;
 import android.support.annotation.WorkerThread;
 
 public class SimpleAsyncTask {
-    private Runnable background_runnable;
-
     public SimpleAsyncTask(Runnable background_runnable) {
-        this.background_runnable = background_runnable;
         Task task = new Task();
-        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, background_runnable);
     }
 
-    private class Task extends AsyncTask<Void, Void, Void> {
-        protected long canvas_id;
-
+    private class Task extends AsyncTask<Runnable, Void, Void> {
         @Override
         @WorkerThread
-        @CallSuper
-        protected Void doInBackground(Void... v) {
+        protected Void doInBackground(Runnable... runnable) {
             Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
-            background_runnable.run();
+            runnable[0].run();
             return null;
         }
 
         @Override
         @UiThread
-        @CallSuper
         protected void onProgressUpdate(Void... v) {
         }
 
         @Override
         @UiThread
-        @CallSuper
         protected void onPreExecute() {
         }
 
         @Override
         @UiThread
-        @CallSuper
         protected void onPostExecute(Void v) {
         }
 
         @Override
         @UiThread
-        @CallSuper
         protected void onCancelled() {
         }
     }

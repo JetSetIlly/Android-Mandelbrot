434278a5fd11c23cec74edaa1f7155be03dc1ec9
steve <steve@galactica.localdomain>
2016-08-08 11:49:37 +0100

    * added some new initialisation signatures to SimpleAsyncTask    - now able to supply a cancelled runnable in addition to completion runnable

--- app/src/main/java/jetsetilly/tools/SimpleAsyncTask.java
+++ app/src/main/java/jetsetilly/tools/SimpleAsyncTask.java
@@ -6,17 +6,42 @@ import android.support.annotation.UiThread;
 import android.support.annotation.WorkerThread;
 
 public class SimpleAsyncTask {
+    private Runnable background_runnable;
+    private Runnable completion_runnable;
+    private Runnable cancelled_runnable;
+
     public SimpleAsyncTask(Runnable background_runnable) {
-        Task task = new Task();
-        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, background_runnable);
+        init(background_runnable, null, null);
     }
 
-    private class Task extends AsyncTask<Runnable, Void, Void> {
+    public SimpleAsyncTask(Runnable background_runnable, Runnable completion_runnable) {
+       init(background_runnable, completion_runnable, null);
+    }
+
+    public SimpleAsyncTask(Runnable background_runnable, Runnable completion_runnable, Runnable cancelled_runnable) {
+        init(background_runnable, completion_runnable, cancelled_runnable);
+    }
+
+    public SimpleAsyncTask(Runnable background_runnable, Runnable completion_runnable, boolean run_complete_when_cancelled) {
+        /* completion runnable is run when task is cancelled */
+        init(background_runnable, completion_runnable, run_complete_when_cancelled?completion_runnable:null);
+    }
+
+    private void init(Runnable background_runnable, Runnable completion_runnable, Runnable cancelled_runnable) {
+        this.background_runnable = background_runnable;
+        this.completion_runnable = completion_runnable;
+        this.cancelled_runnable = cancelled_runnable;
+
+        Task task = new Task();
+        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    private class Task extends AsyncTask<Void, Void, Void> {
         @Override
         @WorkerThread
-        protected Void doInBackground(Runnable... runnable) {
+        protected Void doInBackground(Void... v) {
             Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
-            runnable[0].run();
+            background_runnable.run();
             return null;
         }
 
@@ -33,11 +58,13 @@ public class SimpleAsyncTask {
         @Override
         @UiThread
         protected void onPostExecute(Void v) {
+            if (completion_runnable != null) completion_runnable.run();
         }
 
         @Override
         @UiThread
         protected void onCancelled() {
+            if (cancelled_runnable != null) cancelled_runnable.run();
         }
     }
 }
